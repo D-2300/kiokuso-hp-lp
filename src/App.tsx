c66aa4c7cb799ac730b60e548ef4715c3bc0511d
D-2300 <d.roots001@gmail.com>
2026-03-22 14:01:26 +0900

    Create ScrollToTop component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import HPApp from "./hp/HPApp";
 import LPPage from "./lp/LPPage";
+import ScrollToTop from "./shared/ScrollToTop";
 
 function BodyBackground() {
   const location = useLocation();
@@ -18,6 +19,7 @@ function AppRoutes() {
   return (
     <>
       <BodyBackground />
+      <ScrollToTop />
       <Routes>
         <Route path="/lp" element={<LPPage />} />
         <Route path="/*" element={<HPApp />} />
